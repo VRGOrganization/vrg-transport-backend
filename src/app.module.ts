36db07e4ff1a3077205ecd4bef2f371c9c31e7e5
Rafael Miotte <miotterafael@gmail.com>
2026-03-11 11:29:05 -0300

    criando a base dos primeiros modulos da api backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentModule } from './student/student.module';
+import { EmployeeModule } from './employee/employee.module';
+import { LicenseModule } from './license/license.module';
 
 @Module({
   imports: [
@@ -16,6 +19,9 @@ import { AppService } from './app.service';
         `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST || 'localhost'}:${process.env.MONGODB_PORT || '27017'}/${process.env.MONGODB_DATABASE}?authSource=admin`,
     ),
     UsersModule,
+    StudentModule,
+    EmployeeModule,
+    LicenseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
