686909d5ce7de58d986d43a7eefc65e2a6b0361b
Gustavo Jacob <gustavogustavoribeiro2014@gmail.com>
2026-03-11 11:49:27 -0300

    chore(database): adiciona segunda conexão MongoDB para banco de imagens

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './user/user.module';
+import { ImagesModule } from './images/images.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -15,7 +16,15 @@ import { AppService } from './app.service';
       process.env.MONGODB_URI ||
         `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST || 'localhost'}:${process.env.MONGODB_PORT || '27017'}/${process.env.MONGODB_DATABASE}?authSource=admin`,
     ),
+    MongooseModule.forRoot(
+      process.env.MONGODB_IMAGE_URI ||
+        `mongodb://${process.env.MONGODB_IMAGE_USER}:${process.env.MONGODB_IMAGE_PASSWORD}@${process.env.MONGODB_IMAGE_HOST || 'localhost'}:${process.env.MONGODB_IMAGE_PORT || '27018'}/${process.env.MONGODB_IMAGE_DATABASE}?authSource=admin`,
+      {
+        connectionName: 'images',
+      },
+    ),
     UsersModule,
+    ImagesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
